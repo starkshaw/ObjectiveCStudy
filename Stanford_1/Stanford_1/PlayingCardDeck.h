8c28f6a045b9e1dbcb28c6e13b327279a8856450
Zhenbang “Stark” Xiao <ttxzb@hotmail.com>
2015-07-13 13:39:19 +0800

    Updated Stanford_1 Card Game.

---
+++ Stanford_1/Stanford_1/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Stanford_1
+//
+//  Created by Zhenbang Xiao on 13/07/2015.
+//  Copyright (c) 2015 Zhenbang Xiao. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Stanford_1/Stanford_1/PlayingCardDeck.m
@@ -0,0 +1,31 @@
+//
+//  PlayingCardDeck.m
+//  Stanford_1
+//
+//  Created by Zhenbang Xiao on 13/07/2015.
+//  Copyright (c) 2015 Zhenbang Xiao. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype) init
+{
+    self = [super init];
+    
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+            }
+        }
+    }
+    
+    return self;
+}
+
+@end
